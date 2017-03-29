db495c270c7d6a75dd46755b25596568806bfacb
darsh <darsh.patel0@gmail.com>
2017-03-29 12:24:59 -0400

    database create

---
+++ WebApplication1/src/java/rest/Message.java
@@ -0,0 +1,80 @@
+package rest;
+
+
+import java.text.SimpleDateFormat;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author c0683339
+ */
+public class Message {
+        private int id;
+    private String title;
+    private String contents;
+    private String author;
+    SimpleDateFormat dt=new  SimpleDateFormat("");
+
+    public Message() {
+   
+    }
+      public Message(JsonObject json){ 
+      id = json.getInt("productId"); 
+         title = json.getString("name"); 
+         contents = json.getString("contents"); 
+          author= json.getString("author"); 
+     } 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+   
+
+   public JsonObject toJSON() { 
+        return Json.createObjectBuilder() 
+                 .add("tId",id ) 
+                 .add("name", title) 
+                 .add("contents", contents) 
+                 .add("author",author) 
+                 .build(); 
+     } 
+
+    
+    
+}
--- WebApplication1/src/java/rest/MessageController.java
+++ WebApplication1/src/java/rest/MessageController.java
@@ -5,23 +5,91 @@
  */
 package rest;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
-import javax.jms.Message;
+import javax.json.Json;
 import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 
 /**
  *
  * @author c0683339
  */
 public class MessageController {
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:MM:SS.sssxxx");
     List<Message> messages;
 
     public MessageController() {
-        
+        messages = new ArrayList<>();
+
     }
-    public JsonArray getAllJson(){
+
+    public JsonArray getAllJson() {
+
+        JsonArrayBuilder json = Json.createArrayBuilder();
+        for (Message m : messages) {
+            json.add(m.toJSON());
+        }
+        return json.build();
+    }
+
+    public Message getById(int id) {
+
+        for (Message m : messages) {
+            if (m.getId() == id) {
+                return m;
+            }
+        }
         return null;
     }
+
+    public JsonObject addJson(JsonObject json) {
+        Message m = new Message(json);
+        messages.add(m);
+        return m.toJSON();
+    }
     
+   public boolean deleteByid(int id){
+       
+       Message m=getById(id);
+       if(m!=null)
+       {
+           messages.remove(m);
+           return true;
+           
+       }
+       else{
+           
+           return false;
     
-}
+       }
+        private final static String studentNumber = "C0683339";
+
+    /**
+     * Utility method used to create a Database Connection
+     *
+     * @return the Connection object
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String server = "ipro.lambton.on.ca";
+        String username = studentNumber + "-java";
+        String password = studentNumber;
+        String database = username;
+        String jdbc = String.format("jdbc:mysql://%s/%s", server, database);
+        return DriverManager.getConnection(jdbc, username, password);
+    }
+   }
+   
+   
+   
+   }
+          
